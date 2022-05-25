e841efb6f5d8f83cf0598b07014e36e04c75fb0c
MShafiMS <shahedali734@gmail.com>
2022-05-25 17:37:42 -0600

    update

--- index.js
+++ index.js
@@ -26,12 +26,8 @@ function verifyJWT(req, res, next) {
   next();
 }
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.htqi5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hx93b.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
   try {
     await client.connect();
